599188159dcb2ade48f933ae52e9a86653ea33c0
Lachlan Miller <lachlan.miller.1990@outlook.com>
2022-09-19 13:45:22 +1000

    init

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,11 @@
+<script lang="ts" setup>
+defineProps<{
+  label: string;
+}>();
+</script>
+  
+<template>
+  <button type="button" :title="label" :aria-label="label">
+    <slot />
+  </button>
+</template>
